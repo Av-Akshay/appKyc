147973138eeb6cd05ad1e73121c868a8dd767586
Av-Akshay <akshuchauhan1jan@gmail.com>
2024-12-11 11:32:38 +0530

    chore: chnage the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,63 +1,82 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
-import kycLogo from "../assets/Images/kycLogo.png";
+import { logo } from "../utils/icons";
 
 const Navbar = () => {
   return (
-    <div className="bg-[rgba(0,0,0,0.2)] h-[10vh] w-full flex items-center">
-      <div className="w-11/12 m-auto flex items-center justify-between ">
-        <div>
-          <Link to="/">
-            <img
-              className="h-12 w-12 rounded-full"
-              src={kycLogo}
-              alt=" Kyc Logo"
-            />
-          </Link>
+    <div className="navbar justify-between min-h-[10vh] ">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+          >
+            <li className="bg-transparent">
+              <NavLink>Item 1</NavLink>
+            </li>
+            <li>
+              <NavLink>Parent</NavLink>
+              <ul className="p-2">
+                <li>
+                  <NavLink>Submenu 1</NavLink>
+                </li>
+                <li>
+                  <NavLink>Submenu 2</NavLink>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <NavLink>Item 3</NavLink>
+            </li>
+          </ul>
         </div>
-        <div className="flex items-center gap-6 text-xl font-serif font-bold">
-          <NavLink
-            className={({ isActive }) =>
-              isActive ? "text-light" : "text-black"
-            }
-            to={"/"}
-          >
-            Home
-          </NavLink>
-          <NavLink
-            className={({ isActive }) =>
-              isActive ? "text-light" : "text-black"
-            }
-            to={"/about"}
-          >
-            About
-          </NavLink>
-          <NavLink
-            className={({ isActive }) =>
-              isActive ? "text-light" : "text-black"
-            }
-            to={"/contact"}
-          >
-            Contact
-          </NavLink>
-          <NavLink
-            className={({ isActive }) =>
-              isActive ? "text-light" : "text-black"
-            }
-            to={"/services"}
-          >
-            Services
-          </NavLink>
-          <NavLink
-            className={({ isActive }) =>
-              isActive ? "text-light" : "text-black"
-            }
-            to={"/sign-up"}
-          >
-            Signup
-          </NavLink>
-        </div>
+        <Link className="btn btn-ghost text-xl z-10 absolute">
+          <img className="w-full h-12" src={logo} alt="Logo" />
+        </Link>
       </div>
+      <div className="navbar-center hidden lg:flex mr-5">
+        <ul className="menu-horizontal px-1 gap-8">
+          <li className="bg-transparent">
+            <NavLink className={"font-poppinsSemibold text-slate-800"}>
+              Item 1
+            </NavLink>
+          </li>
+          <li className="bg-transparent">
+            <NavLink className={"font-poppinsSemibold text-slate-800"}>
+              Item 2
+            </NavLink>
+          </li>
+          <li className="bg-transparent">
+            <NavLink className={"font-poppinsSemibold text-slate-800"}>
+              Item 3
+            </NavLink>
+          </li>
+          <li className="bg-transparent">
+            <NavLink className={"font-poppinsSemibold text-slate-800"}>
+              Item 4
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+      {/* <div className="navbar-end">
+        <a className="btn">Button</a>
+      </div> */}
     </div>
   );
 };
