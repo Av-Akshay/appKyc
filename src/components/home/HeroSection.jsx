3e904d73c74ff42943e1c8bb2e65194fd2ebaba3
Av-Akshay <akshuchauhan1jan@gmail.com>
2024-12-18 15:33:29 +0530

    make hero section responsive

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -11,16 +11,18 @@ const HeroSection = () => {
       className="relative clip-path-polygon w-full flex items-center justify-center h-[90vh] p-10 bg-cover bg-no-repeat bg-center"
     >
       <div className="flex items-center justify-center flex-col">
-        <p className="font-poppinsBold text-white text-5xl">Welcome To</p>
-        <h1 className="font-poppinsBold text-them text-9xl text-shadow-lg">
+        <p className="font-poppinsBold text-white text-5xl max-md:text-3xl">
+          Welcome To
+        </p>
+        <h1 className="text-center font-poppinsBold text-them text-9xl text-shadow-lg max-md:text-6xl">
           APP KYC
         </h1>
-        <p className="font-poppinsBold text-5xl text-white capitalize">
+        <p className="font-poppinsBold text-5xl text-white capitalize text-center max-md:text-3xl">
           web & app development company
         </p>
       </div>
       <img
-        className="absolute left-1/2 bottom-2 -translate-x-1/2"
+        className="absolute left-1/2 bottom-2 -translate-x-1/2 max-sm:bottom-5"
         src={mouse}
         alt="mouse"
       />
