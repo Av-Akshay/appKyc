d8f0aef2ab088d0cbab68007a4054efa8f1dd6f0
Av-Akshay <akshuchauhan1jan@gmail.com>
2024-12-18 15:50:16 +0530

    chor: make serviceSection of home page responsive

--- src/components/home/ServiceCard.jsx
+++ src/components/home/ServiceCard.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 
-const ServiceCard = ({ title, text, pic }) => {
+const ServiceCard = ({ title, text, pic, className }) => {
   return (
-    <div className="flex flex-col items-center justify-start gap-5 w-4/5 m-auto">
-      <img src={pic} alt={title} />
+    <div
+      className={`flex flex-col items-center justify-start gap-5 w-4/5 m-auto ${className}`}
+    >
+      <img className="max-sm:w-28" src={pic} alt={title} />
       <div className="flex items-center flex-col gap-5">
         <h1 className="text-[#000000] font-poppinsSemibold text-2xl">
           {" "}
--- src/components/home/ServiceSection.jsx
+++ src/components/home/ServiceSection.jsx
@@ -5,13 +5,13 @@ import ServiceCard from "./ServiceCard";
 
 const ServiceSection = () => {
   return (
-    <div className="pb-36 relative">
+    <div className="pb-36 relative max-md:pb-10">
       <div className=" relative">
         <div className=" absolute rotate-90 left-1/2 -top-20 -translate-x-1/2  w-[6%] border-dotted border-2 border-black">
           {" "}
         </div>
-        <div className="w-1/2 mx-auto flex items-center justify-center my-40 ">
-          <h1 className=" text-balance font-semibold text-center text-3xl text-slate-950">
+        <div className="w-1/2 mx-auto flex items-center justify-center my-40 max-sm:my-28 ">
+          <h1 className=" text-balance font-semibold text-center text-3xl text-slate-950 max-sm:text-xl">
             {" "}
             We are one of the most effective Web & App Development Company{" "}
           </h1>
@@ -20,7 +20,7 @@ const ServiceSection = () => {
           {" "}
         </div>
       </div>
-      <div className="relative grid grid-cols-3 grid-rows-1">
+      <div className="relative grid grid-cols-3 grid-rows-1 max-md:grid-cols-2 max-md:grid-rows-2 max-[500px]:grid-cols-1 max-[500px]:grid-rows-3   ">
         <ServiceCard
           pic={uiUx}
           text="Our Design Thinking team conceptualizes intuitive, engaging, and 
@@ -28,6 +28,7 @@ const ServiceSection = () => {
           offers seamless User Experience (UX) across the web 
           and mobile devices for your audience."
           title={"UI/UX Design"}
+          className={"h-[25rem]"}
         />
         <ServiceCard
           pic={webDevelopment}
@@ -35,8 +36,12 @@ const ServiceSection = () => {
                 But We are Passionate about creating a 
                 Successful Brand for You!!!"
           title={"Web Development"}
+          className={"h-[25rem]"}
         />
         <ServiceCard
+          className={
+            "max-md:col-start-1 max-md:col-end-3 max-md:row-start-2 h-[25rem] max-[500px]:col-start-1 max-[500px]:col-end-2 max-[500px]:row-start-3 "
+          }
           pic={appDevelopment}
           text="Nurture Your Vision, Magnify Your Brand & Increase 
                 The Quality Of You App With Us."
