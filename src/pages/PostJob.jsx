c6b24a5796be6089fced45b2bed75214e8eaab86
Av-Akshay <akshuchauhan1jan@gmail.com>
2024-12-24 12:36:03 +0530

    feat: add private routing functionality in post jobs form page

--- src/pages/PostJob.jsx
+++ src/pages/PostJob.jsx
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 import Input from "../components/Input";
 import usePostJobs from "../hooks/usePostJobs";
+import useToken from "../hooks/useToken";
 
 const PostJob = () => {
   const { errors, handleSubmit, message, register, submitForm } = usePostJobs();
+
+  const navigate = useNavigate();
+  const { token } = useToken();
+  useEffect(() => {
+    if (!token) {
+      navigate("/admin-console");
+    }
+  }, []);
+
   return (
     <div className="bg-section-background flex flex-col gap-5 items-center justify-center w-full py-16">
-      <h1 className="text-5xl text-white font-poppinsSemibold ">Post Jobs</h1>
-      <div className="w-3/5 p-10 border-white border-2 rounded-2xl mb-10 max-sm:w-11/12">
+      {message ? (
+        <h1 className="text-4xl font-bold  text-red-500 mt-10 font-serif ">
+          {message}
+        </h1>
+      ) : (
+        <h1 className="text-4xl font-bold  text-white mt-10 font-serif ">
+          Post Job
+        </h1>
+      )}
+      <div className="w-3/5 p-10 border-white border-2 shadow-lg shadow-white rounded-2xl mb-10 max-sm:w-11/12">
         <form
           className="flex flex-col gap-3"
           onSubmit={handleSubmit(submitForm)}
